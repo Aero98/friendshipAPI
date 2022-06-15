e99b5f276c67512efa1b5ae42825407709afcbf0
Aero98 <fikrimex31@gmail.com>
2022-06-15 23:30:43 +0700

    commit on 'main'

--- db/index.js
+++ db/index.js
@@ -1,13 +1,28 @@
-const { Pool } = require('pg')
+// const { Pool } = require('pg')
+
+// const pool = new Pool({
+//     user:'postgres',
+//     password: 'chester',
+//     database: 'api_friend',
+//     host: 'localhost',
+//     port: 5000
+// })
+
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// }
+
+const Pool = require("pg").Pool;
+require(".env").config();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
-    user:'postgres',
-    password: 'chester',
-    database: 'api_friend',
-    host: 'localhost',
-    port: 5000
-})
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-}
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+module.exports = pool;
